52775aa1c9c2b194c274be32615dfe2fbdac0160
ozgencmardi <ozgencmardi@gmail.com>
2022-12-03 16:42:39 +0000

    JS updated

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
 
 
 var topic = "HTML";
